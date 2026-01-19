48818a990e331aea1b4e4598501c08bb01697c41
s1010803 <s1010803@gm.pu.edu.tw>
2026-01-19 12:41:48 +0800

    complete sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,11 +3,12 @@ import bcrypt from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashPassword = bcrypt.hashSync(password, 10);
-  const newUser = new User({ username, email, hashPassword });
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    await newUser.save();
+    const savedUser = await newUser.save();
+    console.log("資料庫回傳的完整物件:", savedUser);
     res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
